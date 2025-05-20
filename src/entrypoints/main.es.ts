b2c03dce8ad5dee3defcfcc67ed17b98d0e8d987
Marc Pomar <boyander@boyander-m3-mbp.local>
2025-05-20 17:49:20 +0200

    fix: add iife and es bundle

---
+++ src/entrypoints/main.es.ts
@@ -0,0 +1,6 @@
+import { FaableAuthClient } from "../FaableAuthClient";
+import { Session, User } from "../lib/types";
+import { AuthError } from "../lib/errors";
+import { createClient } from "../createClient";
+
+export { Session, User, FaableAuthClient, AuthError, createClient };
